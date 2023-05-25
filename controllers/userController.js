09b8deb0aa25b8dadf4b2bf6b7e9b609e18b5f41
Danar Ramadhya <danar7978@gmail.com>
2023-05-25 18:55:37 +0700

    commit #3
    add docker config

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,37 +4,34 @@ const { Response, Error } = require("../helpers/response");
 class UserController {
   static home(req, res, next) {
     try {
-      res.render("html/home");
+      // render home page ejs
+      res.status(200).render("html/home");
     } catch (error) {
       next(error);
     }
   }
   static async loginUser(req, res, next) {
     try {
+      // extract username from req.body in page
       const { username } = req.body;
       const dataUser = await Users.findOne({
         where: { username: username },
       });
+
+      // if not exists create a new username
       if (!dataUser) {
         const createUser = await Users.create({
           username: username,
         });
-        req.session.userID = createUser.id;
-        req.session.save(() => {
-          return res.redirect("api/user");
-        });
+        return res.status(201).redirect(`/api/user/${createUser.id}`);
       }
+
+      // if exists go to api/user 
       if (dataUser) {
         const dataList = await Lists.findAll({
           where: { userID: dataUser.id },
         });
-        req.session.username = dataUser.username;
-        req.session.dataList = dataList;
-        req.session.userID = dataUser.id;
-        console.log(req.session)
-        req.session.save(() => {
-          return res.redirect("api/user");
-        });
+        return res.status(200).redirect(`/api/user/${dataUser.id}`);
       }
     } catch (error) {
       next(error);
@@ -42,43 +39,67 @@ class UserController {
   }
   static async userPage(req, res, next) {
     try {
-      const username = req.session.username;
-      const dataList = req.session.dataList;
+      // search for user data
+      const userData = await Users.findOne({
+        where: { id: req.params.id }
+      })
 
-      // Render the user view with the data
-      res.render("html/user", { username, dataList });
-    } catch (error) {
-      next(error);
-    }
-  }
-  static async createPage(req, res, next) {
-    try {
-      res.render("html/create");
-    } catch (error) {
-      next(error);
-    }
-  }
-  static async removePage(req, res, next) {
-    try {
-      res.render("html/remove");
+      // search for list's user data and view it in ejs
+      const listData = await Lists.findAll({
+        where: { userID: req.params.id }
+      })
+      res.status(200).render("html/user", {data: listData, username: userData.username, id: userData.id});
     } catch (error) {
       next(error);
     }
   }
   static async addList(req, res, next) {
     try {
+      // extract list from req.body
       const { list } = req.body;
+      const userData = await Users.findOne({
+        where: { id: req.params.id }
+      })
+
+      // create list
       const addList = await Lists.create({
-        userID: id,
+        userID: req.params.id,
         list: list,
       });
-      res.redirect("html/user", { data: addList });
+
+      // find all list to view in ejs
+      const listData = await Lists.findAll({
+        where: { userID: req.params.id }
+      })
+      res.status(200).render("html/user", {data: listData, username: userData.username, id: userData.id});
     } catch (error) {
       next(error);
     }
   }
-  static async editList(req, res, next) {
+  static async deleteList(req, res, next) {
     try {
+      // extract num input from req.body & convert the value to a number
+      const { num } = req.body;
+      // find all the list and delete the data if the num is valid
+      const dataDelete = await Lists.findAll({
+        where: {userID: req.params.id}
+      })
+      if (dataDelete.length >= num) {
+      const listToDelete = dataDelete[num - 1];
+      await listToDelete.destroy();
+      const userData = await Users.findOne({
+        where: { id: req.params.id }
+      });
+
+      // find all list again to view in ejs
+      const listData = await Lists.findAll({
+        where: { userID: req.params.id }
+      });
+      res.status(200).render("html/user", {data: listData, username: userData.username, id: userData.id});
+      
+      } else {
+        res.status(400).send("Invalid num")
+      }
     } catch (error) {
       next(error);
     }
